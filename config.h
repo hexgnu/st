0de5e6ad1e9ebc182fbef5a5ed8c392f161971fd
Matthew Kirk <matt@matthewkirk.com>
2019-05-07 08:07:40 -0700

    feat: use awesome and source code pro

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:weight=80:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Source Code Pro,Awesome:weight=80:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
